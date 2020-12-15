e9653bcb0b4e9243c07b4037ec87086aa9aa5c90
filefolder <rpickle@gmail.com>
2020-12-15 17:02:08 +1100

    Update arpicker.c

--- obspy/signal/src/arpicker.c
+++ obspy/signal/src/arpicker.c
@@ -55,10 +55,10 @@ int ar_picker(float *tr, float *tr_1, float *tr_2, int ndat, float sample_rate, 
 
 #define EXIT(code) \
     free(buff1); \
-    //free(buff1_s); \
+    free(buff1_s); \
     //free(buff2); \
     //free(buff3); \
-    free(buff4); \
+    //free(buff4); \
     //free(buff4_s); \
     free(f_error); \
     free(b_error); \
