88c044f4f652625bfe5927e001a603cdceac2457
filefolder <rpickle@gmail.com>
2020-12-15 14:05:27 +1100

    Update arpicker.c

--- obspy/signal/src/arpicker.c
+++ obspy/signal/src/arpicker.c
@@ -185,8 +185,9 @@ int ar_picker(float *tr, float *tr_1, float *tr_2, int ndat, float sample_rate, 
         EXIT(12);
     }
 
-    for(i=0;i<=MAX(0,(i1-nlta));i++){ //added MAX and equal sign to <=
+    for(i=0;i<(i1-nlta);i++){ 
         for(j=(i+nlta-nsta);j<(i+nlta);j++){
+            fprintf("A) i+ntla = %d \n",i+ntla)            
             buf_sta[i+nlta] += buff3[j]/(float)nsta;
         }
         for(j=(i);j<(i+nlta);j++){
